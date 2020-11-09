70f2f6621854c0399132be279511177c3fd8fde7
Nicholas Morlin <nicholas_morlin@hotmail.com>
2020-11-08 23:02:18 -0300

    Criando repository e service

---
+++ src/main/java/com/nicholasmorlin/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.nicholasmorlin.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nicholasmorlin.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
