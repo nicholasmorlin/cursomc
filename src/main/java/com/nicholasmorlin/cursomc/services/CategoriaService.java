9ef1e1b8099a0f5b538b9d967c55f7dc943d66f8
Nicholas Morlin <nicholas_morlin@hotmail.com>
2020-11-09 22:12:41 -0300

    Ajustes no endpoint /categorias/{id} e Tratamento de erros ExceptionHandler

--- src/main/java/com/nicholasmorlin/cursomc/services/CategoriaService.java
+++ src/main/java/com/nicholasmorlin/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nicholasmorlin.cursomc.domain.Categoria;
 import com.nicholasmorlin.cursomc.repositories.CategoriaRepository;
+import com.nicholasmorlin.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	} 
 
 }
